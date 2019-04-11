941e32450a30980c8e2b8987d2b4cbb3d5875913
Drake Foltz <GeenDutchman@byu.net>
2019-04-10 22:10:16 -0600

    My attempt...

--- driver.py
+++ driver.py
@@ -3,8 +3,16 @@
 import numpy as np
 import time
 
-from TSPSolver import TSPSolver
+from TSPSolver import TSPSolution
 
+def find_not_visited(visited, ncities): # O(n)
+    results = []
+    for i in range(ncities):
+        if i not in visited:
+            results.append(i)
+    if len(results) == ncities and len(visited) > 0:
+        results = [visited[0]]
+    return results
 
 def fancy(matrix):
     start_time = time.time()
@@ -12,38 +20,69 @@ def fancy(matrix):
     ncities = len(matrix[0])
 
     original_matrix = matrix
-    cost_lookup = [{} for i in range(ncities)]
 
-    # Inital cost lookup
-    for i in range(1, ncities):
-        cost_lookup[0][(i, frozenset())] = original_matrix[i, 0]
+    layers = []
+    next_layer = []
+    not_visited = find_not_visited([0], ncities)
+    if len(not_visited) > 0:
+        for i in not_visited:
+            path_dict = {}
+            path_dict[0] = []
+            if original_matrix[0][i] != np.inf:
+                path_dict[0].append((i, original_matrix[0][i]))
+        if len(path_dict) > 0:
+            next_layer.append(path_dict)
+    if len(next_layer) > 0:
+        layers.append(next_layer)
 
-    # 1st layer
-    for i in range(1, ncities):
-        for key in cost_lookup[0]:
-            if key[0] == i:
-                pass
-            else:
-                cost_lookup[1][(i, key[1].union(
-                    [key[0]]))] = original_matrix[i][key[0]] + cost_lookup[0][key]
+    # layers[0] = [ {[A]:[(B,7)]}, {[A]:[(C,3)]}, {[A]:[(D,12)]} ]
+    # layers[1] = [ {[AB]:[(C,13),(D,21)]}, {[AC]:[(B,11),(D,9)]}, {[AD]:[(B,15),(C,17)]} ]
+    # layers[2] = [ {[ABC]:[(D,19)]}, {[ACD]:[(B,12)]}, {[ADB]:[(C,21)]} ]
+    # layers[3] = [ {[ABCD]:[(A,28)]}, {[ACDB]:[(A,15)]}, {[ADBC]:[(A,26)]} ]
+    #  end layers
 
-    # >1 layers
-    for i in range(2, ncities):
-        for j in range(1, ncities):
-            values = np.array([])
-            for key in cost_lookup[i-1]:
-                if key[0] == j:
-                    pass
-                else:
-                    cost = original_matrix[j][key[0]
-                                                ] + cost_lookup[i-1][key]
-                    np.append(values, cost)
+    while len(layers) > 0:
+        next_layer = []
+        for a_dict in layers[-1]:
+            for path in a_dict.keys():
+                min_tuple = min(a_dict[path], key = lambda a_tuple: a_tuple[1])
+                if min_tuple[1] == np.inf:
+                    continue
+                next_path = path.copy()
+                next_path.append(min_tuple[0])
+                path_dict = {}
+                path_dict[next_path] = []
+                not_visited = find_not_visited(path, ncities)
+                for i in not_visited:
+                    path_dict[next_path].append((i, min_tuple[1] + original_matrix[path[-1]][i]))
+                if len(path_dict) > 0:
+                    next_layer.append(path_dict)
+        if len(next_layer) == 0:
+            break
+        layers.append(next_layer)
+
+    solution = 'no solution'
+    foundTour = False
+    if len(layers) > 0:
+        min_path = []
+        min_length = np.inf
+        for a_dict in layers[-1]:
+            for path in a_dict.keys():
+                a_min_tuple = min(a_dict[path], key=lambda a_tuple: a_tuple[1])
+                if a_min_tuple[1] < min_length:
+                    min_length = a_min_tuple[1]
+                    min_path = path.copy()
+                    min_path.append(a_min_tuple[0])
+        if len(min_path) > 0 and min_path[0] == min_path[-1]:
+            foundTour = True
+            solution = TSPSolution(min_path)
+
 
     end_time = time.time()
-    results['cost'] = np.inf
+    results['cost'] = solution.cost if foundTour else np.inf
     results['time'] = end_time - start_time
-    results['count'] = np.inf
-    results['soln'] = cost_lookup
+    results['count'] = len(layers)
+    results['soln'] = solution if foundTour else None
     results['max'] = None
     results['total'] = None
     results['pruned'] = None
